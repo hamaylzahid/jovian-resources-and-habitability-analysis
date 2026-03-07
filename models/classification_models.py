2c20c42053e93a28d90634ee9d421768c066583c
Hamayl Zahid <hamaylzahid@gmail.com>
2026-03-07 20:18:19 +0500

    Add function to train classification models

--- models/classification_models.py
+++ models/classification_models.py
@@ -1,3 +1,18 @@
-
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
+
+def train_classification_models(X_train,y_train):
+
+    logistic_model = LogisticRegression()
+
+    rf_model = RandomForestClassifier(
+        n_estimators=100,
+        max_depth=78,
+        random_state=42
+    )
+
+    logistic_model.fit(X_train,y_train)
+
+    rf_model.fit(X_train,y_train)
+
+    return logistic_model,rf_model
