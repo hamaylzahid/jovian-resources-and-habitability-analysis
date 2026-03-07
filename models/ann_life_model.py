efebba56f0fbc8ba64768a31949f8be05dedf4b5
Hamayl Zahid <hamaylzahid@gmail.com>
2026-03-07 20:17:51 +0500

    initial commit

--- models/ann_life_model.py
+++ models/ann_life_model.py
@@ -1,2 +1,18 @@
+import numpy as np
 
-# ANN model logic
+def estimate_life_longevity(row):
+
+    if row['Probability of Life Presence (%)'] < 10:
+        return 0
+
+    elif row['Radiation levels (mSv/year)'] > 0.1:
+        return np.random.randint(1,50)
+
+    elif row['Temperature range (C)'] < -150:
+        return np.random.randint(10,500)
+
+    elif row['Water presence (Y/N)'] == 1:
+        return np.random.randint(100,10000)
+
+    else:
+        return np.random.randint(50,1000)
